663fd25c476f0b61bcb5291ea6c329b375928b8f
Joblamp <joblamp@hotmail.com>
2020-11-06 09:55:03 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 // JavaScript Document
+var button = document.querySelector('.menubutton');
+button.addEventListener('click', myFunction);
+
 function myFunction() {
   var x = document.querySelector("#myLinks");
   if (x.style.display === "block") {
